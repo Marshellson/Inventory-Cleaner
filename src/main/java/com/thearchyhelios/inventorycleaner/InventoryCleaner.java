28fe064d8476bb1eb69a1b165c8a0585dfdf9fb6
Thearchy.Helios <jade-topspin.0v@icloud.com>
2022-09-12 18:49:47 +0200

    First Commit

---
+++ src/main/java/com/thearchyhelios/inventorycleaner/InventoryCleaner.java
@@ -0,0 +1,31 @@
+package com.thearchyhelios.inventorycleaner;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = InventoryCleaner.MODID, name = InventoryCleaner.NAME, version = InventoryCleaner.VERSION)
+
+public class InventoryCleaner {
+    public static final String MODID = "inventorycleaner";
+    public static final String NAME = "Inventory Cleaner";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+}
+
